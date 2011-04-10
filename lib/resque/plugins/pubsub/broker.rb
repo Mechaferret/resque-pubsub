fb133e78976f10026fd9ec724017627d60356167
Ryan Moran <ryan.moran@gmail.com>
2011-04-10 01:19:57 -0700

    class method rework

--- lib/resque/plugins/pubsub/broker.rb
+++ lib/resque/plugins/pubsub/broker.rb
@@ -7,20 +7,25 @@ module Resque
         # Returns a top-level Redis connection so that the broker can distribute messages
         # across namespaces. If none has been created, will
         # create a new one using information from the Resque connection.
-        def self.redis
-          return @redis if @redis
-          client_to_copy = Resque.redis.client
-          @redis = Redis.new(:host => client_to_copy.host, :port => client_to_copy.port, :thread_safe => true, :db => client_to_copy.db)
-        end
 
-        def self.perform(topic, message)
-          subscribers = Exchange.redis.smembers("#{topic}_subscribers")
-          subscribers.each do |s|
-            sinfo = JSON.parse(s)
-            puts "[Broker] distributing to #{sinfo.inspect}"
-            Broker.redis.sadd("#{sinfo['namespace']}:queues", "fanout:#{topic}")
-            Broker.redis.rpush("#{sinfo['namespace']}:queue:fanout:#{topic}", { :class => sinfo['class'], :args => [message] }.to_json)
+        class << self
+
+          def redis
+            return @redis if @redis
+            client_to_copy = Resque.redis.client
+            @redis = Redis.new(:host => client_to_copy.host, :port => client_to_copy.port, :thread_safe => true, :db => client_to_copy.db)
           end
+
+          def perform(topic, message)
+            subscribers = Exchange.redis.smembers("#{topic}_subscribers")
+            subscribers.each do |s|
+              sinfo = JSON.parse(s)
+              puts "[Broker] distributing to #{sinfo.inspect}"
+              Broker.redis.sadd("#{sinfo['namespace']}:queues", "fanout:#{topic}")
+              Broker.redis.rpush("#{sinfo['namespace']}:queue:fanout:#{topic}", { :class => sinfo['class'], :args => [message] }.to_json)
+            end
+          end
+
         end
 
       end
