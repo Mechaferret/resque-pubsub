11a0beddef46eebea588ef62d403ff8a318a0fbe
Ryan Moran <ryan.moran@gmail.com>
2011-04-10 14:34:23 -0700

    more idiomatic

--- test/fixtures/test_custom_subscriber.rb
+++ test/fixtures/test_custom_subscriber.rb
@@ -3,15 +3,17 @@ class TestCustomSubscriber
 
   subscribe 'test_custom_topic', :reader_method => :simple
 
-  @@last_message = nil
+  class << self
 
-  def self.simple(message)
-    puts "[#{self.to_s}] got test custom topic message: #{message.inspect}"
-    @@last_message = message
-  end
+    def simple(message)
+      puts "[#{self.to_s}] got test custom topic message: #{message.inspect}"
+      @last_message = message
+    end
 
-  def self.last_message
-    @@last_message
+    def last_message
+      @last_message
+    end
+
   end
 
 end
