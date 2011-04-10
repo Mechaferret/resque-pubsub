2894fc932a3bbf2f98542714a8102c7390be83f2
Ryan Moran <ryan.moran@gmail.com>
2011-04-09 20:26:03 -0700

    more cleanup

--- lib/resque/plugins/pubsub/broker.rb
+++ lib/resque/plugins/pubsub/broker.rb
@@ -1,10 +1,8 @@
 module Resque
   module Plugins
-    #
-    # pubsub broker
-    #
     module Pubsub
       class Broker
+
         @queue = :messages
         # Returns a top-level Redis connection so that the broker can distribute messages
         # across namespaces. If none has been created, will
@@ -12,19 +10,19 @@ module Resque
         def self.redis
           return @redis if @redis
           client_to_copy = Resque.redis.client
-          @redis = Redis.new(:host => client_to_copy.host, :port => client_to_copy.port,
-            :thread_safe => true, :db => client_to_copy.db)
+          @redis = Redis.new(:host => client_to_copy.host, :port => client_to_copy.port, :thread_safe => true, :db => client_to_copy.db)
         end
-        
+
         def self.perform(topic, message)
           subscribers = Exchange.redis.smembers("#{topic}_subscribers")
-          subscribers.each {|s|
+          subscribers.each do |s|
             sinfo = JSON.parse(s)
             puts "distributing to #{sinfo.inspect}"
             Broker.redis.sadd("#{sinfo['namespace']}:queues", "fanout:#{topic}")
             Broker.redis.rpush("#{sinfo['namespace']}:queue:fanout:#{topic}", {:class=> sinfo["class"], :args=>[message]}.to_json)
-          }
+          end
         end
+
       end
     end
   end
